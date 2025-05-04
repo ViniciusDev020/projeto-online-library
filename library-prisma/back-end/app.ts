55bfc2240f5f83114546e99f0a19fc75526fdf5f
vini <vinicius.l.r.s@outlook.com>
2025-05-03 23:27:46 -0300

    cors

--- library-prisma/back-end/app.ts
+++ library-prisma/back-end/app.ts
@@ -9,13 +9,7 @@ import { autenticacao } from "./middlewares/autenticacao.ts";
 const app = express();
 const port = 3001;
 
-app.use(
-  cors({
-    origin: ["https://projeto-online-library-znqy.vercel.app/login"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use("/login", login);
