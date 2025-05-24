f066194cbb6ecb277e965a015f190d2da9e219d5
vini <vinicius.l.r.s@outlook.com>
2025-05-23 23:55:23 -0300

    feat: add perfil

--- back-end/src/prisma/seed.ts
+++ back-end/src/prisma/seed.ts
@@ -2,28 +2,106 @@ import { PrismaClient } from "@prisma/client";
 import { randomUUID } from "crypto";
 const prisma = new PrismaClient();
 async function main() {
-  const livroHistoria = await prisma.livro.upsert({
-    where: { id: randomUUID() },
-    update: {},
-    create: {
-      id: randomUUID(),
-      name: "Livro de história",
-      description: "Descrição do livro de história.",
-      author: {},
-    },
+  const normalUser = await prisma.user.createMany({
+    data: [
+      {
+        id: "105",
+        email: "joao@email.com",
+        name: "João",
+        password: "joao123",
+        perfil: "usuario",
+      },
+      {
+        id: "108",
+        email: "pedro@email.com",
+        name: "Pedro",
+        password: "pedro123",
+        perfil: "admin",
+      },
+    ],
   });
-  console.log({ livroHistoria });
 
   const autor = await prisma.author.upsert({
     where: { id: randomUUID() },
     update: {},
     create: {
-      id: randomUUID(),
+      id: "230",
+      age: 23,
       name: "Pedro Lopez",
+      nacionality: "Brasileiro",
     },
   });
 
-  console.log({ autor });
+  const livros = await prisma.livro.createMany({
+    data: [
+      {
+        id: randomUUID(),
+        name: "Livro de história",
+        description: "Descrição do livro de história.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de fantasia",
+        description: "Descrição do livro de fantasia.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de matemática",
+        description: "Descrição do livro de matemática.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de ciência",
+        description: "Descrição do livro de ciência.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de geografia",
+        description: "Descrição do livro de geografia.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de ciência",
+        description: "Descrição do livro de ciência.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de fantasia",
+        description: "Descrição do livro de fantasia.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de ciência",
+        description: "Descrição do livro de ciência.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de história",
+        description: "Descrição do livro de história.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de ciência",
+        description: "Descrição do livro de ciência.",
+        authorId: "230",
+      },
+      {
+        id: randomUUID(),
+        name: "Livro de história",
+        description: "Descrição do livro de história.",
+        authorId: "230",
+      },
+    ],
+  });
 }
 main()
   .then(async () => {
