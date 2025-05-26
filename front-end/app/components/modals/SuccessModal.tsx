fffb48a6569bf41cb37f25510b9c7db71d253469
vini <vinicius.l.r.s@outlook.com>
2025-05-25 23:54:48 -0300

    fix: removido bg dark modal

--- front-end/app/components/modals/SuccessModal.tsx
+++ front-end/app/components/modals/SuccessModal.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/esm/Modal";
 
 export default function SuccessModal(props) {
   const { message, show, hideSuccessModal } = props;
 
   return (
-    <Modal
-      show={show}
-      onHide={hideSuccessModal}
-      contentClassName="text-center"
-      className="bg-dark"
-    >
+    <Modal show={show} onHide={hideSuccessModal} contentClassName="text-center">
       <Modal.Header closeButton></Modal.Header>
       <Modal.Body>{...message}</Modal.Body>
     </Modal>
